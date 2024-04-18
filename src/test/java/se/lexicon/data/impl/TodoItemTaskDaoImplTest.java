0f6dc11918d13042203477c1679cdcc97cca26a7
Camelia Ciuca <camelia1414@gmail.com>
2024-04-18 15:54:07 +0200

    Test ToDo Item task

---
+++ src/test/java/se/lexicon/data/impl/TodoItemTaskDaoImplTest.java
@@ -0,0 +1,16 @@
+package se.lexicon.data.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.lexicon.model.TodoItemTask;
+
+public class TodoItemTaskDaoImplTest {
+    private TodoItemTaskDaoImpl testObject;
+    @BeforeEach
+    public void setUp() {
+        testObject = new TodoItemTaskDaoImpl();
+    }
+    @Test
+    public void testCreateTodoItemTask() {
+    }
+}
